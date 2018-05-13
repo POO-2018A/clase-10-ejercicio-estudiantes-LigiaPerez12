5a8e0be4dca20036708f04b307c2efeff482cc12
LigiaPerez12 <38448479+LigiaPerez12@users.noreply.github.com>
2018-05-13 17:59:55 -0500

    Add files via upload

---
+++ Estudiantes.java
@@ -0,0 +1,54 @@
+package estudiantes;
+
+/**
+ *
+ * @author bayon
+ */
+public class Estudiantes {
+
+   private String nombre;
+   private String apellido;
+   private String cedula;
+   private String[] materias;
+
+    public Estudiantes(String nombre, String apellido, String cedula) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+    }
+   
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+   
+    public String[] getMaterias() {
+        return materias;
+    }
+
+    public void setMaterias(String[] materias) {
+        this.materias = materias;
+    }    
+    
+}
